e7bb56bc4f5b7604de57976e28d6b606feb6d903
Jacob <jacobcwebber@gmail.com>
2018-05-07 17:38:50 -0400

    changed requirements.txt

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import User
+from models import *
 
 connection = pymysql.connect(host='localhost',
                              user='Jacob',
