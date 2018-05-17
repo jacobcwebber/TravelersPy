f5c2bc8f7100e63492013615f71f0cddbcff4caf
Jacob <jacobcwebber@gmail.com>
2018-05-16 21:17:29 -0400

    finishing up login and registration under new system

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField, DecimalField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Email, EqualTo, ValidationError
+from app.models import User
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired()])
@@ -8,12 +9,18 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
-class RegisterForm(FlaskForm):
-    email = StringField('', validators=[InputRequired()], render_kw={"placeholder": "Email"})
-    first_name = StringField('', validators=[InputRequired()], render_kw={"placeholder": "First name"})
-    last_name = StringField('', validators=[InputRequired()], render_kw={"placeholder": "Last name"}) 
-    password = PasswordField('', validators=[InputRequired()], render_kw={"placeholder": "Password"})
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired()])
+    first_name = StringField('First Name', validators=[InputRequired(), Email()])
+    last_name = StringField('Last Name', validators=[InputRequired()]) 
+    password = PasswordField('Password', validators=[InputRequired()])
+    password_repeat = PasswordField('Repeat Password', validators=[InputRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
 
 class CountryForm(FlaskForm):
     name = StringField('Name', validators=[InputRequired()])
--- app/routes.py
+++ app/routes.py
@@ -1,69 +1,35 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from flask_login import current_user, login_user, logout_user, login_required
-from app import app
-from app.forms import LoginForm, RegisterForm
+from werkzeug.urls import url_parse
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/index', methods=['POST', 'GET'])
 def index():
+    if current_user.is_authenticated:
+        return redirect(url_to('destinations'))
     login_form = LoginForm()
-    register_form = RegisterForm()
+    registration_form = RegistrationForm()
 
-    if request.method == 'POST':
-        if request.form['submit'] == "Create account":
-            email = register_form.email.data
-            first = register_form.firstName.data
-            last = register_form.lastName.data
-            password = sha256_crypt.encrypt(str(register_form.password.data))
-
-            cur = connection.cursor()
-            cur.execute("INSERT INTO users(FirstName, LastName, Password, Email) "
-                        "VALUES (%s, %s, %s, %s, %s)"
-                        , (first, last, password, email))
-
-            connection.commit()
-            cur.close()
-
-            flash('Congratulations! You are now registered.', 'success')
-
+    if registration_form.validate_on_submit():
+        user = User(first_name=form.first_name.data, last_name=form.last_name.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    elif login_form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password.')
             return redirect(url_for('login'))
-        elif request.form['submit'] == "Login":
-            email = login_form.email
-            password_attempt = login_form.password
-            remember_me = login_form.remember_me
-
-            cur = connection.cursor()
-            result = cur.execute("SELECT * "
-                                "FROM users "
-                                "WHERE Email = %s"
-                                , email)
-            user = cur.fetchone()
-            cur.close()
-
-            if result > 0:
-                password = user['Password']
-                userId = user['UserID'] 
-                firstname = user['FirstName']
-                isAdmin = user['IsAdmin']
-
-                if sha256_crypt.verify(password_attempt, password):
-                    
-                    session['user'] = userId
-                    session['logged_in'] = True
-                    session['is_admin'] = isAdmin
-
-                    return redirect(url_for('destinations'))
-
-                else:
-                    error = 'Invalid login. Please try again.'
-                    return render_template('login.html', error=error)
-                cur.close()
-
-            else:
-                error = "User not found."
-                return render_template('login.html', error=error)
-
-    return render_template('index.html', login_form=login_form, register_form=register_form)
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('index.html', login_form=login_form, registration_form=registration_form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -76,7 +42,10 @@ def login():
             flash('Invalid username or password.')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', form=form)
 
 @app.route('/logout')
